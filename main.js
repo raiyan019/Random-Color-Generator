fa331c83acef380fb6a7d3f9de7ed548bcb29ed1
Raiyan Ahmed <raiyan00719@gmail.com>
2024-12-21 14:25:50 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+const getColor = () => {
+// Hex Code 
+const randomNumber = Math.floor(Math.random() * 16777215);
+const randomCode = "#" + randomNumber.toString(16);
+document.body.style.backgroundColor =randomCode;
+document.getElementById("ColorCode").innerText = randomCode;
+
+navigator.clipboard.writeText(randomCode)
+}
+
+document.getElementById("btn").addEventListener(
+    "Press",
+    getColor
+)
+    
+
+
+getColor();
